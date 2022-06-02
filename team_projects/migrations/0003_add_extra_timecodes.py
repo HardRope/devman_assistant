90acd5ab33027366141881094eee4cdf76415111
ilyashirko <ilyashirko@gmail.com>
2022-06-02 20:26:49 +0300

    add more timecodes

---
+++ team_projects/migrations/0003_add_extra_timecodes.py
@@ -0,0 +1,38 @@
+import random
+
+from django.db import migrations
+
+def add_timecodes(apps, schema_editor):
+    Mentor = apps.get_model('team_projects', 'Mentor')
+    Student = apps.get_model('team_projects', 'Student')
+    Level = apps.get_model('team_projects', 'Level')
+    Project = apps.get_model('team_projects', 'Project')
+    AvailableTimecode = apps.get_model('team_projects', 'AvailableTimecode')
+    Timecode = apps.get_model('team_projects', 'Timecode')
+    
+    project = Project.objects.get(title='DVMN TEST')
+    students = project.students.all()
+    available_timecodes = project.available_timecodes.all()
+    for _ in range(len(students)):
+        timecode, _ = Timecode.objects.get_or_create(
+            timecode=random.choice(available_timecodes),
+            project=project,
+            student=students[_],
+        )
+        timecode.save()
+        timecode, _ = Timecode.objects.get_or_create(
+            timecode=random.choice(available_timecodes),
+            project=project,
+            student=students[_],
+        )
+        timecode.save()
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('team_projects', '0002_input_default_values'),
+    ]
+
+    operations = [
+        migrations.RunPython(add_timecodes)
+    ]
