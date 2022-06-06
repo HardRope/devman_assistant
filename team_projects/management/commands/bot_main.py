687075bc12f9c3dc743fa9daff4b6f8444a14987
PaSeRouS <paser96@mail.ru>
2022-06-06 16:49:07 +0300

    Change '/start' in bot

--- team_projects/management/commands/bot_main.py
+++ team_projects/management/commands/bot_main.py
@@ -1,27 +1,48 @@
 import os
 
 from .admin import admin_menu
+from .db_processing import get_actual_projects, get_mentors, get_participants
 from .project_manager import pm_menu
 from .student import student_menu
 
 
 def start(update, context):
     '''Запуск бота, определение роли, вывод нужного меню'''
-
-    students = os.getenv("STUDENTS")
-    project_managers = os.getenv("PROJECT_MANAGERS")
     admins = os.getenv("ADMINS")
 
     user_name = update.effective_user.name
+    user_id = update.effective_user.id
 
     context.bot.send_message(
         text=f"Добро пожаловать в бота Devman, {user_name}",
         chat_id=update.effective_chat.id,
     )
 
-    if user_name in students:
-        student_menu(update, context)
-    if user_name in project_managers:
-        pm_menu(update, context)
+    projects = get_actual_projects()
+    students_id = []
+    mentors_id = []
+
+    for project in projects:
+        students = get_participants(project)
+        mentors = get_mentors()
+
+        for student in students:
+            students_id.append(student.telegram_id)
+
+        for mentor in mentors:
+            mentors_id.append(mentor.telegram_id)
+
+    print(students_id)
+    print(mentors_id)
+
     if user_name in admins:
         admin_menu(update, context)
+    elif user_id in mentors_id:
+        pm_menu(update, context)
+    elif user_id in students_id:
+        student_menu(update, context)
+    else:
+        context.bot.send_message(
+            text=f"Сейчас у вас нет актуальных проектов",
+            chat_id=update.effective_chat.id,
+        )
